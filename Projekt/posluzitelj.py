fd0d84fe49a6ae480155f50001b6407c653064a0
Jelena Dasic <jdasic1@gmail.com>
2022-06-25 12:43:49 +0200

    socketi rade - ostalo nista

--- Projekt/posluzitelj.py
+++ Projekt/posluzitelj.py
@@ -3,6 +3,8 @@ from threading import *
 import konfiguracijska_datoteka
 
 AUTH_MESSAGE = "Autentikacija preko RSA kljuceva...".encode()
+HEADER =  b' ' *2048
+FORMAT = 'utf-8'
 class ChatThread(Thread):
     def __init__(self,con):
         Thread.__init__(self)
@@ -18,13 +20,17 @@ class ChatThread(Thread):
                 print('Client: ',recData)
 
 server = socket(AF_INET, SOCK_STREAM)
-server.bind(('127.0.0.1', 1234))
+server.bind(('127.0.0.1', 4321))
 server.listen(2)
 conn, addr = server.accept()
 sender = ChatThread(conn)
 sender.setName('Sender')
 receiver=ChatThread(conn)
 receiver.setName('Receiver')
-konfiguracijska_datoteka.AutentifikacijaRSA()
+
+msg_length = conn.recv(HEADER).decode(FORMAT)
+server.recv(conn.recv(private_key_client).decode(FORMAT))
+
+konfiguracijska_datoteka.AutentikacijaRSA()
 sender.start()
 receiver.start()
